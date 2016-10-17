84f32f6d9fe57b94f2afecd229a81de5358f4c2c
Dani.W10.clase <dlealreyes@gmail.com>
2016-10-17 14:32:31 +0200

    Falta las validaciones 17/10/2016

---
+++ 04-Grid/04-Grid/MainPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using _04_Grid.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace _04_Grid
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private clsPersona persona;
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void BtnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+
+            persona=new clsPersona();
+
+            //Validacion Nombre
+            if (String.IsNullOrEmpty(txtNombre.Text))
+            {
+                lblErrorNombre.Text="El nombre no puede estar vacío";
+            }
+            else
+            {
+                lblErrorNombre.Text = "";
+                persona.nombre = txtNombre.Text;
+            }
+
+            //Validacion Apellidos
+            if (String.IsNullOrEmpty(txtApellidos.Text))
+            {
+                lblErrorApellidos.Text = "El Apellido no puede estar vacío";
+            }
+            else
+            {
+                lblErrorApellidos.Text = "";
+                persona.apellido = txtApellidos.Text;
+            }
+            //Validacion de la fecha
+            if (Fecha.Date == null)
+            {
+                lblErrorFecha.Text = "La fecha no puede estar Vacía";
+            }
+            else if(Fecha.Date > DateTime.Now)
+            {
+                lblErrorFecha.Text = "La fecha debe ser anterior a la actual.";
+            }
+            else
+            {
+                lblErrorFecha.Text = "";
+                persona.fechaNacimiento = Fecha.Date;
+
+            }
+
+            lblPersona.Text = persona.ToString();
+            
+
+        }
+    }
+}
---
+++ 04-Grid/04-Grid/Models/clsPersona.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Grid.Models
+{
+    class clsPersona
+    {
+        //propiedades y Getters/Setters
+        public String nombre { get; set; }
+        public String apellido { get; set; }
+        public Nullable <DateTimeOffset> fechaNacimiento { get; set; }
+
+        //constructores
+        public clsPersona(String nombre,String apellido,DateTime fechaNacimiento)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.fechaNacimiento = fechaNacimiento;
+        }
+
+        public clsPersona()
+        {
+            this.nombre = null;
+            this.apellido = null;
+            this.fechaNacimiento = null;
+        }
+        override
+        public String ToString()
+        {
+            return ("Nombre: "+this.nombre+" , Apellidos: "+this.apellido+", Fecha de Nacimiento: "+this.fechaNacimiento.ToString());
+        }
+    }
+}
