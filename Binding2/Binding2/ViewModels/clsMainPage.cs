81e9a97c88cf1aa5adc72a22c09ce7e98adc3176
Dani.W10.clase <dlealreyes@gmail.com>
2016-11-14 14:03:38 +0100

    Falta AdaptativeTrigger Master/detail

---
+++ Binding2/Binding2/ViewModels/clsMainPage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _Binding2.Models;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace Binding2.ViewModels
+{
+    public class clsMainPage:INotifyPropertyChanged
+    {
+        private clsPersona _personaSeleccionada;
+        public ObservableCollection<clsPersona> lista { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public clsMainPage()
+        {
+           lista = new clsListado().list;
+        }
+
+        public clsPersona  personaSeleccionada
+        {
+            get
+            {
+                return _personaSeleccionada;
+            }
+
+            set
+            {
+                _personaSeleccionada = value;
+                OnPropertyChanged("personaSeleccionada");
+
+            }
+        }
+
+        // Create the OnPropertyChanged method to raise the event
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+
+    }
+}
