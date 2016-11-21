7874081c865f8a32be4a759887f199f37eae5fb6
Dani.W10.clase <dlealreyes@gmail.com>
2016-11-21 14:35:33 +0100

    User Control Seguir

--- UserControl1/UserControl1/MainPage.xaml.cs
+++ UserControl1/UserControl1/MainPage.xaml.cs
@@ -11,6 +11,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // La plantilla de elemento Página en blanco está documentada en http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -22,9 +23,57 @@ namespace UserControl1
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private static int columna=0;
+        private static int fila=0;
+
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        public void insertarFicha(String nombre,String apellidos,String uri)
+        {
+            //Creamos el UserControl
+            BitmapImage image = new BitmapImage(new Uri(uri, UriKind.Absolute));
+
+            UserControls.fichaAlumno newAlumno = new UserControls.fichaAlumno();
+            newAlumno.Foto = image;
+            newAlumno.StudentName = nombre;
+            newAlumno.StudentFirstName = apellidos;
+
+            newAlumno.HorizontalContentAlignment = HorizontalAlignment.Center;
+         
+
+            //Añadimos una fila
+            RowDefinition row = new RowDefinition();
+            row.Height = Windows.UI.Xaml.GridLength.Auto;
+            Grid grid = gridPrincipal;
+            RowDefinitionCollection coleccion = gridPrincipal.RowDefinitions;
+            coleccion.Add(row);
+
+
+
+            Button boton = new Button();
+            boton.Visibility = Visibility;
+            boton.Content = "BOTOOOOOOOOOOOOOOOOOOOOOOOOOOOOON";
+            boton.IsEnabled = true;
+            //Image imagen = new Image();
+
+
+
+
+
+            //grid.Children.Add(column);
+            columna += 1;
+            fila += 1;
+
+
+
+        }
+
+        private void AppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.insertarFicha("Dani", "leal", "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAftAAAAJDI3ZGNjMGU1LWY5NTEtNGQwOC04MGZiLWE3MmEyOGFkMzA1Yg.jpg");
         }
     }
 }
--- UserControl1/UserControl1/UserControls/fichaAlumno.xaml.cs
+++ UserControl1/UserControl1/UserControls/fichaAlumno.xaml.cs
@@ -11,6 +11,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
@@ -25,18 +26,35 @@ namespace UserControl1.UserControls
         }
 
 
-
-        public String StudentName
+        public string StudentName
         {
-            get { return (String)GetValue(StudentNameProperty); }
+            get { return (string)GetValue(StudentNameProperty); }
             set { SetValue(StudentNameProperty, value); }
         }
+        public string StudentFirstName
+        {
+            get { return (string)GetValue(StudentFirstNameProperty); }
+            set { SetValue(StudentFirstNameProperty, value); }
+        }
+
+        public ImageSource Foto
+        {
+            get { return (ImageSource)GetValue(FotoProperty); }
+            set { SetValue(FotoProperty, value); }
+        }
+
 
         // Using a DependencyProperty as the backing store for StudentName.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty StudentNameProperty =
-            DependencyProperty.Register("StudentName", typeof(String),null ,new PropertyMetadata(String.Empty));
+            DependencyProperty.Register("StudentName", typeof(string), typeof(fichaAlumno), new PropertyMetadata("Nombre"));
 
+        public static readonly DependencyProperty StudentFirstNameProperty =
+          DependencyProperty.Register("StudentFirstName", typeof(string), typeof(fichaAlumno),   new PropertyMetadata("Apellidos"));
 
+        public static readonly DependencyProperty FotoProperty =
+          DependencyProperty.Register("Foto",  typeof(ImageSource), typeof(fichaAlumno),
+              new PropertyMetadata(new BitmapImage(new Uri("ms-appx://_UserContol/fotos/foto.jpg"))));
+    
 
 
     }
